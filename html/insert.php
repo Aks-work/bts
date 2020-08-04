9750b61b42ede20c83509229b81ee0d89243649c
Aks-work <69187808+Aks-work@users.noreply.github.com>
2020-08-04 13:13:40 +0530

    Add files via upload

---
+++ html/insert.php
@@ -0,0 +1,22 @@
+<?php
+$name=$_POST['name'];
+$fanname=$_POST['fanname'];
+$password=$_POST['password'];
+$email=$_POST['email'];
+$number=$_POST['number'];
+$gender=$_POST['gender'];
+$country=$_POST['country'];
+i
+
+	$conn= new mysql_i('localhost','root','','regi');
+	if($conn->connect_error){
+		die('connection failed :'.$conn->connect_error);
+	  	}else{
+	  		$stmt=$conn->prepare("insert into registar(name,fanname,password,email,number,gender,country) values(?,?,?,?,?,?,?)");
+	  		$stmt->bind_param("ssssiss",$name,$fanname,$password,$email,$number,$gender,$country);
+	  		$stmt->execute();
+	  		echo "registration successfull";
+	  		$stmt->close();
+            $conn->close();
+	  	}
+	  	?>
